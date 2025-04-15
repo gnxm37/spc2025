4f6b65c701d1edc9862408f15558e2230485c1d7
gnxm37 <133202877+gnxm37@users.noreply.github.com>
2025-04-15 23:57:23 +0900

    user modal crud fetch commit

--- 3.JS/8.Node/9.express/public/homework.js
+++ 3.JS/8.Node/9.express/public/homework.js
@@ -5,6 +5,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const ageInput = document.getElementById('ageInput');
     const userTable = document.getElementById('userTable');
 
+    const editmodal = document.getElementById('editmodal');
+    const removemodal = document.getElementById('removemodal');
+
+    const openModal = document.getElementById('openModal');
+    const closeButton = document.getElementById('closeButton');
+    const changeButton = document.getElementById('changeButton');
+
+    const changeageInput = document.getElementById('changeageInput');
+    const changeusernameInput = document.getElementById('changeusernameInput');
+    const changeusernicknameInput = document.getElementById('changeusernicknameInput');
+    
+    const noButton = document.getElementById('noButton');
+    const yesButton = document.getElementById('yesButton');
+
+    openModal.addEventListener('click', () => {
+        editmodal.style.display = 'flex';
+    });
+
+    closeButton.addEventListener('click', () => {
+        editmodal.style.display = 'none';
+    });
+
+    // 
     updateTable();
     form.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -15,24 +38,24 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('/users', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({usernickname, username, age})
+            body: JSON.stringify({ usernickname, username, age })
         })
 
-        usernicknameInput.value='';
-        usernameInput.value='';
-        ageInput.value='';
+        usernicknameInput.value = '';
+        usernameInput.value = '';
+        ageInput.value = '';
 
         updateTable();
     });
 
-    function createButton(text, clickHandler){
+    function createButton(text, clickHandler) {
         const button = document.createElement('button');
         button.textContent = text;
         button.addEventListener('click', clickHandler);
         return button;
     }
 
-    function updateTable(){
+    function updateTable() {
         userTable.innerHTML = "";
 
         fetch('/users')
@@ -57,44 +80,53 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function editUser(userId) {
-        const newName = prompt('수정할 이름을 입력하세요.');
-        // 코드를 구현해볼것... PUT 을 어떻게 부를것인가??
-        fetch(`/users/${userId}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ 
-                username: newName
-            })
+        editmodal.style.display = 'flex';
+
+        closeButton.addEventListener('click', () => {
+            editmodal.style.display = 'none';
         })
-            .then(res => { // 나머지 모든 부분에서도 이런식으로 에러처리를 해야 좋음.
-                if (!res.ok) throw new Error('수정 실패');
-                alert('수정 성공');
-                updateTable();
+        changeButton.addEventListener('click', (event) => {
+            event.preventDefault();
+            fetch(`/users/${userId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    usernickname: changeusernicknameInput.value,
+                    username: changeusernameInput.value,
+                    age: changeageInput.value
+                })
             })
-            .catch(error => {
-                alert('수정 중 오류 발생');
-            });
+                .then(res => { // 나머지 모든 부분에서도 이런식으로 에러처리를 해야 좋음.
+                    if (!res.ok) throw new Error('수정 실패');
+                    updateTable();
+                    removemodal.style.display = 'none';
+                })
+                .catch(error => {
+                    alert('수정 중 오류 발생');
+                });
+        })
     }
 
     function deleteUser(userId) {
-        const confirmDelete = confirm('정말로 삭제하시겠습니까?');
-        if (confirmDelete) {
+        removemodal.style.display = 'flex';
+
+        noButton.addEventListener('click', () => {
+            removemodal.style.display = 'none';
+        })
+        yesButton.addEventListener('click', (event) => {
+            event.preventDefault();
             fetch(`/users/${userId}`, {
                 method: 'DELETE'
             })
                 .then(res => {
                     if (!res.ok) throw new Error('삭제 실패');
                     updateTable();
-                    alert('삭제 성공');
+                    removemodal.style.display = 'none';
                 })
                 .catch(error => {
                     console.error('삭제 중 오류 발생: ', error);
                     alert('삭제 중 오류 발생');
-                })
-
-        } else {
-            alert('장난치지 마시오...');
-        }
-    }
-
+                });
+        });
+    };
 });
